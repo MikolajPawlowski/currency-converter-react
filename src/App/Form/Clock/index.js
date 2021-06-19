4e40aa88939d77981c696aad234575bd6e639689
Mikolaj <Pawlowskiem@gmail.com>
2021-06-19 22:40:44 +0200

    clock js added

---
+++ src/App/Form/Clock/index.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+
+const Clock = () => {
+    const [currentDate, setCurrent] = useState(newDate());
+    const stringDate = currentDate.toLocaleDateString(
+        undefined,
+        {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+        },
+    );
+
+    useEffect(() => {
+        setInterval(() => {
+            setCurrentDate(new Date());
+        }, 1000);
+        
+        return () => {
+            clearInterval(setCurrentDate);
+        };
+    }, []);
+
+    return (
+        <p className="clock">
+            Dziś jest {stringDate}
+        </p>
+    )
+};
+
+export default Clock;
