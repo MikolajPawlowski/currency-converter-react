c23563462ee991fc91b76aa8da8be1e76af1a8ed
Mikolaj <Pawlowskiem@gmail.com>
2021-06-23 18:20:39 +0200

    toFixed changed

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,13 +1,13 @@
 import "./style.css";
 
-export const Result = ({ result }) => (
+export const Result = ({ result, currency }) => (
     <p className="result">
         {result !== undefined && (
             <>
                 {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
                 {" "}
                 <strong>
-                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                    {result.targetAmount.toFixed(currency === "Bitcoin" ? 2 : 6)}&nbsp;{result.currency}
                 </strong>
             </>
         )}
